e4e21302a0f37bbaa9062bdd82dd21657f01a11c
Petr Trofimov <petrofimov@yandex.ru>
2013-01-20 17:16:38 +0300

    cleaned test.php

--- test.php
+++ test.php
@@ -1,37 +1,15 @@
 <?php
 require_once(__DIR__ . '/vendor/autoload.php');
 
-use Graph\Method;
-use Graph\Object;
-
-$method = new Method(function () {
-    return 1;
-});
-
-$method = new Method($method, function () {
-    return 2;
-});
-
-$method = new Method($method, function () {
-    return 3;
-});
-
-var_dump([
-    $method(),
-    $method->parent(),
-    $method->parent->parent(),
-    $method->parent->parent,
-]);
-
-$o = new Object();
+$o = new Graph\Object();
 $o->getNumber = function () {
     return $this->property;
 };
 $o->getNumber = function () {
     return $this->parent();
 };
-$o->property='property';
-$o->getProperty=function(){
+$o->property = 'property';
+$o->getProperty = function () {
     return $this->property;
 };
 
